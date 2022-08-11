36cac3b7ae149ae666a6eb3d22cda969ae03618c
ruslanashaari <ruslan.ashaari@gmail.com>
2022-08-12 03:34:30 +0800

    fix: change path to css and images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,15 @@
     <title>User Card - {{ $user->name }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <noscript><link rel="stylesheet" href="{{ asset('css/noscript.css') }}" /></noscript>
 </head>
 <body class="is-preload">
 <div id="wrapper">
     <section id="main">
         <header>
             <span class="avatar">
-                <img src="images/users/{{ $user->id }}.jpg" alt="" />
+                <img src="{{ asset('images/users/' . $user->id . '.jpg') }}" alt="" />
             </span>
             <h1>{{ $user->name ?? 'Not available' }}</h1>
             <p>{{ $user->comments ? nl2br($user->comments) : 'Not available'</p>
