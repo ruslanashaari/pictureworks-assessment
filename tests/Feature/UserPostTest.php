7d24410649a0c3370496f3f8052c381479561660
ruslanashaari <ruslan.ashaari@gmail.com>
2022-08-12 03:34:31 +0800

    test: post request as json

--- tests/Feature/UserPostTest.php
+++ tests/Feature/UserPostTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 
 class UserPostTest extends TestCase
 {
-    public function test_append_comment_existing_user()
+    public function test_append_comment_with_existing_user()
     {
         $user = User::create([
             'name'      =>  'John Doe',
@@ -33,7 +33,7 @@ class UserPostTest extends TestCase
         ]);
     }
 
-    public function test_append_comment_non_existing_user()
+    public function test_append_comment_with_non_existing_user()
     {
         $data = [
             'id'        =>  999,
@@ -44,5 +44,29 @@ class UserPostTest extends TestCase
         $response = $this->post('/user', $data);
         $response->assertStatus(200)
                     ->assertSeeText('No such user (999)');
+    }
+
+    public function test_json_append_comment_with_existing_user()
+    {
+        $user = User::create([
+            'name'      =>  'John Doe',
+            'comments'  =>  'Software Developer'
+        ]);
+
+        $data = [
+            'id'        =>  $user->id,
+            'comments'  =>  ' and Investor',
+            'password'  =>  '720DF6C2482218518FA20FDC52D4DED7ECC043AB'
+        ];
+
+        $response = $this->json('POST', '/user', $data);
+        $response->assertStatus(200)
+                    ->assertViewIs('index');
+
+        $this->assertDatabaseHas('users', [
+            'id'        =>  $user->id,
+            'name'      =>  $user->name,
+            'comments'  =>  $user->comments . ' and Investor',
+        ]);
     }
 }
