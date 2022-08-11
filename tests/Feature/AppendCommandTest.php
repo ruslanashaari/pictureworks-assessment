910a5ffb4316aba32bd1bcf92adf2cda31df30de
ruslanashaari <ruslan.ashaari@gmail.com>
2022-08-12 03:34:30 +0800

    feat: test for artisan command

---
+++ tests/Feature/AppendCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Tests\TestCase;
+
+class AppendCommandTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_with_existing_user()
+    {
+        $user = User::create([
+            'name'      =>  'John Doe',
+            'comments'  =>  'Name is John Doe'
+        ]);
+
+        $this->artisan('user:comment ' . $user->id . ' apple')
+                ->expectsOutput('Ok')
+                ->doesntExpectOutput('No such user (' . $user->id . ')')
+                ->assertExitCode(0);
+    }
+}
